f681d8d8e11204444e378533a30e92685bb1739e
ethanvader <93680550+ethanvader@users.noreply.github.com>
2025-03-04 17:20:28 -0500

    Finally fixed stupid things

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TrebuchetAuton.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+import java.util.List;
+
+@Autonomous()
+public class TrebuchetAuton extends LinearOpMode {
+     private JohnnyTrebuchet johnny6;
+
+     private ElapsedTime runtime=new ElapsedTime();
+
+    @Override
+    public void runOpMode() {
+        johnny6 = new JohnnyTrebuchet(this, JohnnyTrebuchet.Drivetrain.TREBUCHET6);
+        runtime.reset();
+
+        waitForStart();
+
+        double speed = 0.5;
+        int rest = 100;
+    }
+}
